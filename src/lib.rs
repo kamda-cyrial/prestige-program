91da97a21b667e15b816aa622afbb6b60c495ead
Cyrial Kamda <42605286+kamda-cyrial@users.noreply.github.com>
2022-08-22 05:36:30 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use processor::process_instruction;
 use solana_program::entrypoint;
 
 entrypoint!(process_instruction);
-//back to the oldn trials
+//lol, this is super interesting.
 
 
 #[cfg(test)]
